17769e635c1fcb2d71d93d47d2f77428aa7ec0e3
Stefano Gallina <59648879+zenforall@users.noreply.github.com>
2024-03-12 12:03:12 +0100

    Update pageUtil.tsx
    Aggiunta la funzione per ottenere il valore di un elemento in formato Stringa da un Form

--- frontend/src/utils/pageUtil.tsx
+++ frontend/src/utils/pageUtil.tsx
@@ -1,3 +1,12 @@
-export function sum(a: number, b: number): number {
-    return a + b;
+export function getFormElementValueAsString(formElement: HTMLFormElement, elementName: string): string {
+    if (formElement == null) throw new Error("Form element is empty");
+    if (elementName == null) throw new Error("Form element name is empty");
+    if (formElemnt.currentTarget == null) throw new Error("Form element CurrentTarget is empty");
+    if (formElemnt.currentTarget.elements == null) throw new Error("Form element CurrentTarget elements is empty");
+
+    let element = formElement.currentTarget.elements.namedItem(elementName);
+
+    if (element == null) throw new Error("Form element "+elementName+" is not found in Form");
+
+    return ((element as HTMLInputElement).value as string);
 }
