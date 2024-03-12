48a75747209b085dc3234ccd500d8e72ed0f0d86
Stefano Gallina <59648879+zenforall@users.noreply.github.com>
2024-03-12 11:46:45 +0100

    Update loginDTO.tsx
    Aggiunto costruttore

--- frontend/src/dto/loginDTO.tsx
+++ frontend/src/dto/loginDTO.tsx
@@ -7,6 +7,11 @@ export class LoginDTO {
         this.password = "";
     }
 
+    constructor(userName: string, password: string) {
+        this.userName = userName;
+        this.password = password;
+    }
+
     reset() : void {
         this.userName = "";
         this.password = "";
